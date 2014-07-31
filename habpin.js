33ef7353b56c4bcc6f8576163da34d22f6be8108
daviduc <daviduc1969@gmail.com>
2014-07-31 09:03:03 -0700

    july 31 2014 09:02

--- habpin.js
+++ habpin.js
@@ -1,34 +1,5 @@
 ﻿
-(function ($) {
-    $.fn.html5jTabs = function (options) {
-        return this.each(function (index, value) {
-            var obj = $(this), objFirst = obj.eq(index), objNotFirst = obj.not(objFirst);
-            $("#" + objNotFirst.attr("data-toggle")).hide();
-            $(this).eq(index).addClass("active");
-            obj.click(function (evt) {
-                var re = new RegExp(obj.attr("data-toggle"));
-                var arr_toggler = new Array();
-                $('[id]').each(function ()
-                {
-                    if (re.test($(this).attr("id"))) arr_toggler.push($(this).attr("id")); /*add id to array*/
-                });
-                
-                toggler = "#" + obj.attr("data-toggle");
-                togglerRest = $(toggler).parent().find("div");
-                togglerRest.hide();
-                //turn off all tab names
-                $(this).parent("div").find("a").removeClass("active");
-                //turn on only the active tab
-                $(this).addClass("active");
-                arr_toggler.forEach(function (id_name) {
-                    toggler = "#" + id_name;
-                    $(toggler).show();
-                });
-                return false;
-            });
-        });
-    };
-} (jQuery));
+
 
 function onLoadProfile() {
     IN.Event.on(IN, "auth",onLinkedAuth);
